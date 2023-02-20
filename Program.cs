8ed72db0139e59bd34e65d685bcd81b607874620
mermaidseaweav <123306614+mermaidseaweav@users.noreply.github.com>
2023-02-21 00:54:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedFor
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            for (int dep = 1; dep <= 4; dep++)
+            {
+                Console.WriteLine("Department " +dep);
+                Console.WriteLine("**************");
+                for (int emp =1; emp <= 3; emp++)
+                {
+                    Console.WriteLine(" Employee " + emp);
+                }
+                Console.WriteLine("**************");
+            }
+        }
+    }
+}
